0ac65bbc387f863c1be28d948fe9f5a8aa9da368
Yummimi <sarah.tiblin@gmail.com>
2024-04-05 18:45:31 +0200

    Signup init

---
+++ Silicon_Website_App/Controllers/AuthController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Silicon_Website_App.Controllers
+{
+    public class AuthController : Controller
+    {
+        [Route("/signup")]
+        public IActionResult SignUp()
+        {
+            return View();
+        }
+    }
+}
